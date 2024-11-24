838eedf15221a9dd12fccd3f5d6b0c69eea992af
MichaelKumanov <michaelkumanov@gmail.com>
2024-11-24 13:40:45 +0200

    final

--- tests/login test/positivelogin.spec.js
+++ tests/login test/positivelogin.spec.js
@@ -10,41 +10,41 @@ test.describe.only("PositiveTest", () => {
     await page.locator('[data-test="password"]').fill("secret_sauce");
     await page.locator('[data-test="login-button"]').click();
     await expect(page.getByText("Swag Labs")).toBeVisible;
-
+  });
+  test("test 2", async ({ page }) => {
     await page.goto("https://www.saucedemo.com/");
     await expect(page.getByText("Accepted usernames are:")).toBeVisible;
     await page.locator('[data-test="username"]').click();
     await page.locator('[data-test="username"]').type("problem_user");
-    //await page.locator('[data-test="password"]').click();
     await page.locator('[data-test="password"]').fill("secret_sauce");
     await page.locator('[data-test="login-button"]').click();
     await expect(page.getByText("Swag Labs")).toBeVisible;
-
+  });
+  test("test 3", async ({ page }) => {
     await page.goto("https://www.saucedemo.com/");
     await expect(page.getByText("Accepted usernames are:")).toBeVisible;
     await page.locator('[data-test="username"]').click();
     await page
       .locator('[data-test="username"]')
       .type("performance_glitch_user");
-    //await page.locator('[data-test="password"]').click();
     await page.locator('[data-test="password"]').fill("secret_sauce");
     await page.locator('[data-test="login-button"]').click();
     await expect(page.getByText("Swag Labs")).toBeVisible;
-
+  });
+  test("test 5", async ({ page }) => {
     await page.goto("https://www.saucedemo.com/");
     await expect(page.getByText("Accepted usernames are:")).toBeVisible;
     await page.locator('[data-test="username"]').click();
     await page.locator('[data-test="username"]').type("error_user");
-    //await page.locator('[data-test="password"]').click();
     await page.locator('[data-test="password"]').fill("secret_sauce");
     await page.locator('[data-test="login-button"]').click();
     await expect(page.getByText("Swag Labs")).toBeVisible;
-
+  });
+  test("test 123", async ({ page }) => {
     await page.goto("https://www.saucedemo.com/");
     await expect(page.getByText("Accepted usernames are:")).toBeVisible;
     await page.locator('[data-test="username"]').click();
     await page.locator('[data-test="username"]').type("visual_user");
-    //await page.locator('[data-test="password"]').click();
     await page.locator('[data-test="password"]').fill("secret_sauce");
     await page.locator('[data-test="login-button"]').click();
     await expect(page.getByText("Swag Labs")).toBeVisible;
