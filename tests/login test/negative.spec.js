8ab627ae494b9c5d73d076d29c34f9b543f36dae
MichaelKumanov <michaelkumanov@gmail.com>
2024-11-24 14:01:46 +0200

    save changes

--- tests/login test/negative.spec.js
+++ tests/login test/negative.spec.js
@@ -1,107 +1,109 @@
 import { test, expect } from "@playwright/test";
-//
-test("test", async ({ page }) => {
-  await page.goto("https://www.saucedemo.com/");
-  await expect(page.getByText("Accepted usernames are:")).toBeVisible;
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill("locked_out_user");
-  await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill("secret_sauce");
-  await page.locator('[data-test="login-button"]').click();
-  await expect(
-    page.getByText(
-      "Epic sadface: Username and password do not match any user in this service"
-    )
-  ).toBeVisible;
-})
+
+test.describe("Negative Suite", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/");
+  });
+
+  test("test", async ({ page }) => {
+    await expect(page.getByText("Accepted usernames are:")).toBeVisible();
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="username"]').fill("locked_out_user");
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill("secret_sauce");
+    await page.locator('[data-test="login-button"]').click();
+    await expect(
+      page.getByText(
+        "Epic sadface: Username and password do not match any user in this service"
+      )
+    ).toBeVisible();
+  });
 
   test("test 2", async ({ page }) => {
-  await page.goto("https://www.saucedemo.com/");
-  await expect(page.getByText("Accepted usernames are:")).toBeVisible;
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill("standard_user");
-  await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill("seeecret_sauce");
-  await page.locator('[data-test="login-button"]').click();
-  await expect(
-    page.getByText(
-      "Epic sadface: Username and password do not match any user in this service"
-    )
-  ).toBeVisible;
-});
-
-test("test 3", async ({ page }) => {
-  await page.goto("https://www.saucedemo.com/");
-  await expect(page.getByText("Accepted usernames are:")).toBeVisible;
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill("standaard_user");
-  await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill("secret_sauce");
-  await page.locator('[data-test="login-button"]').click();
-  await expect(
-    page.getByText(
-      "Epic sadface: Username and password do not match any user in this service"
-    )
-  ).toBeVisible;
-});
-
-test("test 4", async ({ page }) => {
-await page.goto("https://www.saucedemo.com/");
-await expect(page.getByText("Accepted usernames are:")).toBeVisible;
-await page.locator('[data-test="username"]').click();
-await page.locator('[data-test="username"]').fill("standaard_user");
-await page.locator('[data-test="password"]').click();
-await page.locator('[data-test="password"]').fill("seeecret_sauce");
-await page.locator('[data-test="login-button"]').click();
-await expect(
-  page.getByText(
-    "Epic sadface: Username and password do not match any user in this service"
-  )
-).toBeVisible;
-});
-
-test("test 5", async ({ page }) => {
-  await page.goto("https://www.saucedemo.com/");
-await expect(page.getByText("Accepted usernames are:")).toBeVisible;
-await page.locator('[data-test="username"]').click();
-await page.locator('[data-test="username"]').fill(" ");
-await page.locator('[data-test="password"]').click();
-await page.locator('[data-test="password"]').fill("secret_sauce");
-await page.locator('[data-test="login-button"]').click();
-await expect(
-  page.getByText(
-    "Epic sadface: Username and password do not match any user in this service"
-  )
-).toBeVisible;
-});
-
-test("test 6", async ({ page }) => {
-await page.goto("https://www.saucedemo.com/");
-await expect(page.getByText("Accepted usernames are:")).toBeVisible;
-await page.locator('[data-test="username"]').click();
-await page.locator('[data-test="username"]').fill("standard_user");
-await page.locator('[data-test="password"]').click();
-await page.locator('[data-test="password"]').fill(" ");
-await page.locator('[data-test="login-button"]').click();
-await expect(
-  page.getByText(
-    "Epic sadface: Username and password do not match any user in this service"
-  )
-).toBeVisible;
-});
-
-test("test 7", async ({ page }) => {
-  await page.goto("https://www.saucedemo.com/");
-  await expect(page.getByText("Accepted usernames are:")).toBeVisible;
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill("");
-  await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill("");
-  await page.locator('[data-test="login-button"]').click();
-  await expect(
-    page.getByText(
-      "Epic sadface: Username and password do not match any user in this service"
-    )
-  ).toBeVisible;
-  await page.pause
+    await expect(page.getByText("Accepted usernames are:")).toBeVisible();
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="username"]').fill("standard_user");
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill("seeecret_sauce");
+    await page.locator('[data-test="login-button"]').click();
+    await expect(
+      page.getByText(
+        "Epic sadface: Username and password do not match any user in this service"
+      )
+    ).toBeVisible();
   });
+
+  test("test 3", async ({ page }) => {
+    await expect(page.getByText("Accepted usernames are:")).toBeVisible();
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="username"]').fill("standaard_user");
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill("secret_sauce");
+    await page.locator('[data-test="login-button"]').click();
+    await expect(
+      page.getByText(
+        "Epic sadface: Username and password do not match any user in this service"
+      )
+    ).toBeVisible();
+  });
+
+  test("test 4", async ({ page }) => {
+    await expect(page.getByText("Accepted usernames are:")).toBeVisible();
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="username"]').fill("standaard_user");
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill("seeecret_sauce");
+    await page.locator('[data-test="login-button"]').click();
+    await expect(
+      page.getByText(
+        "Epic sadface: Username and password do not match any user in this service"
+      )
+    ).toBeVisible();
+  });
+
+  test("test 5", async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/");
+    await expect(page.getByText("Accepted usernames are:")).toBeVisible();
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="username"]').fill(" ");
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill("secret_sauce");
+    await page.locator('[data-test="login-button"]').click();
+    await expect(
+      page.getByText(
+        "Epic sadface: Username and password do not match any user in this service"
+      )
+    ).toBeVisible();
+  });
+
+  test("test 6", async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/");
+    await expect(page.getByText("Accepted usernames are:")).toBeVisible();
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="username"]').fill("standard_user");
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill(" ");
+    await page.locator('[data-test="login-button"]').click();
+    await expect(
+      page.getByText(
+        "Epic sadface: Username and password do not match any user in this service"
+      )
+    ).toBeVisible();
+  });
+
+  test("test 7", async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/");
+    await expect(page.getByText("Accepted usernames are:")).toBeVisible();
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="username"]').fill("");
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill("");
+    await page.locator('[data-test="login-button"]').click();
+    await expect(
+      page.getByText(
+        "Epic sadface: Username and password do not match any user in this service"
+      )
+    ).toBeVisible();
+    await page.pause;
+  });
+});
--- tests/login test/positivelogin.spec.js
+++ tests/login test/positivelogin.spec.js
@@ -1,52 +1,50 @@
 import { test, expect } from "@playwright/test";
 //
 test.describe.only("PositiveTest", () => {
-  test("test", async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto("https://www.saucedemo.com/");
-    await expect(page.getByText("Accepted usernames are:")).toBeVisible;
+  });
+  test("test", async ({ page }) => {
+    await expect(page.getByText("Accepted usernames are:")).toBeVisible();
     await page.locator('[data-test="username"]').click();
     await page.locator('[data-test="username"]').type("standard_user");
     await page.locator('[data-test="password"]').click();
     await page.locator('[data-test="password"]').fill("secret_sauce");
     await page.locator('[data-test="login-button"]').click();
-    await expect(page.getByText("Swag Labs")).toBeVisible;
+    await expect(page.getByText("Swag Labs")).toBeVisible();
   });
   test("test 2", async ({ page }) => {
-    await page.goto("https://www.saucedemo.com/");
-    await expect(page.getByText("Accepted usernames are:")).toBeVisible;
+    await expect(page.getByText("Accepted usernames are:")).toBeVisible();
     await page.locator('[data-test="username"]').click();
     await page.locator('[data-test="username"]').type("problem_user");
     await page.locator('[data-test="password"]').fill("secret_sauce");
     await page.locator('[data-test="login-button"]').click();
-    await expect(page.getByText("Swag Labs")).toBeVisible;
+    await expect(page.getByText("Swag Labs")).toBeVisible();
   });
   test("test 3", async ({ page }) => {
-    await page.goto("https://www.saucedemo.com/");
-    await expect(page.getByText("Accepted usernames are:")).toBeVisible;
+    await expect(page.getByText("Accepted usernames are:")).toBeVisible();
     await page.locator('[data-test="username"]').click();
     await page
       .locator('[data-test="username"]')
       .type("performance_glitch_user");
     await page.locator('[data-test="password"]').fill("secret_sauce");
     await page.locator('[data-test="login-button"]').click();
-    await expect(page.getByText("Swag Labs")).toBeVisible;
+    await expect(page.getByText("Swag Labs")).toBeVisible();
   });
   test("test 5", async ({ page }) => {
-    await page.goto("https://www.saucedemo.com/");
-    await expect(page.getByText("Accepted usernames are:")).toBeVisible;
+    await expect(page.getByText("Accepted usernames are:")).toBeVisible();
     await page.locator('[data-test="username"]').click();
     await page.locator('[data-test="username"]').type("error_user");
     await page.locator('[data-test="password"]').fill("secret_sauce");
     await page.locator('[data-test="login-button"]').click();
-    await expect(page.getByText("Swag Labs")).toBeVisible;
+    await expect(page.getByText("Swag Labs")).toBeVisible();
   });
   test("test 123", async ({ page }) => {
-    await page.goto("https://www.saucedemo.com/");
-    await expect(page.getByText("Accepted usernames are:")).toBeVisible;
+    await expect(page.getByText("Accepted usernames are:")).toBeVisible();
     await page.locator('[data-test="username"]').click();
     await page.locator('[data-test="username"]').type("visual_user");
     await page.locator('[data-test="password"]').fill("secret_sauce");
     await page.locator('[data-test="login-button"]').click();
-    await expect(page.getByText("Swag Labs")).toBeVisible;
+    await expect(page.getByText("Swag Labs")).toBeVisible();
   });
 });
